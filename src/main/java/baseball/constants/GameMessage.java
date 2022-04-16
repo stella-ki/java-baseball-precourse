f5c704b5bf83374ab793e3603a16558fac084085
DESKTOP-60RKR9O\neato <neatouo@naver.com>
2022-04-17 00:48:45 +0900

    feat(Game UI) : BaseballGame UI - 게임 시작 후 진행 view 로직 구현 - 게임 종료 후 재진행 여부 view 로직 - User 생성

---
+++ src/main/java/baseball/constants/GameMessage.java
@@ -0,0 +1,14 @@
+package baseball.constants;
+
+public class GameMessage {
+    public static final String MSG_INPUT_BALLS_VALUE = "숫자를 입력해주세요 : ";
+
+    public static final String MSG_GAME_FINISHED = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public static final String MSG_ASK_QUESTION_GAME_FINISH = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String MSG_ASK_ANSWER_YES = "1";
+    public static final String MSG_ASK_ANSWER_NO = "2";
+
+    public static final String SPACE = " ";
+
+}
---
+++ src/main/java/baseball/view/GameMachine.java
@@ -0,0 +1,67 @@
+package baseball.view;
+
+import baseball.constants.ErrorMessage;
+import baseball.constants.GameMessage;
+import baseball.controller.BaseballGame;
+import baseball.domain.player.Player;
+import baseball.domain.player.User;
+import camp.nextstep.edu.missionutils.Console;
+
+public class GameMachine {
+
+    BaseballGame game = new BaseballGame();
+    Player user = new User();
+
+    public void run(){
+        boolean isGameFinished = false;
+
+        game.start();
+
+        while (!isGameFinished){
+            isGameFinished = game();
+        }
+    }
+
+    public boolean game(){
+        game.init();
+        System.out.print(GameMessage.MSG_INPUT_BALLS_VALUE);
+
+        String userBalls = Console.readLine();
+        user.generateBalls(userBalls);
+        game.play(user);
+
+        game.printGameScore();
+
+        if(game.isFinished()){
+            return gameClose();
+        }
+
+        return false;
+    }
+
+    public boolean gameClose(){
+
+        System.out.println(GameMessage.MSG_GAME_FINISHED);
+        if(askUser()){
+            game.start();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean askUser(){
+        System.out.println(GameMessage.MSG_ASK_QUESTION_GAME_FINISH);
+        String answer = Console.readLine();
+
+        if(answer.equals(GameMessage.MSG_ASK_ANSWER_YES)){
+            return true;
+        }
+
+        if(answer.equals(GameMessage.MSG_ASK_ANSWER_NO)){
+            return false;
+        }
+
+        System.out.println(ErrorMessage.ILLEGAL_ANSWER);
+        return askUser();
+    }
+}
