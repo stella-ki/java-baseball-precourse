1e923e1117e97370d9e1acd3b2263fde708576c0
DESKTOP-60RKR9O\neato <neatouo@naver.com>
2022-04-16 13:23:34 +0900

    feat(에러메시지) : ball 검증 에러 메시지 정의 - ball 검증 중 발생할 수 있는 에러 메시지에 대해 미리 정의한다.

---
+++ src/main/java/baseball/constants/ErrorMessage.java
@@ -0,0 +1,8 @@
+package baseball.constants;
+
+public class ErrorMessage {
+    public static final String ILLEGAL_FORMAT_PARAMETER_RANGE = "잘못된 범위의 값을 입력하였습니다.";
+    public static final String ILLEGAL_FORMAT_PARAMETER_LENGTH = "잘못된 길이의 값을 입력하였습니다.";
+
+   
+}
